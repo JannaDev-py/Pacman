fb162870c9d563fa89ac460e84de0da9d3b23c63
JannaDev-py <orlandojannael@gmail.com>
2025-06-15 16:38:57 -0600

    bg with lines

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 const canvas = document.querySelector('canvas');
 const canvasContainer = document.querySelector('div');
-canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 // 2 indicates that theres a coin for pacman
 // 0 indicates empty filed
 // 1 indicates a wall
@@ -14,12 +14,12 @@ const gameboard = [
     [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
     [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
-    [2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2],
-    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-    [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-    [2, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 2],
-    [2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 0, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
@@ -33,10 +33,26 @@ const gameboard = [
 canvas.width = canvasContainer.clientWidth;
 canvas.height = canvasContainer.clientHeight;
 window.addEventListener('resize', () => {
-    console.log('resize');
     canvas.width = canvasContainer.clientWidth;
     canvas.height = canvasContainer.clientHeight;
 });
 function renderGameboard(gameboard) {
+    const widthPiece = canvas.width / gameboard[0].length;
+    const heightPiece = canvas.height / gameboard.length;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    gameboard.forEach((row, rowIndex) => {
+        row.forEach((cell, cellIndex) => {
+            if (cell === 2) {
+                // lest draw a circle
+                ctx.beginPath();
+                const x = (cellIndex * widthPiece) + (widthPiece / 2);
+                const y = (rowIndex * heightPiece) + (heightPiece / 2);
+                ctx.arc(x, y, widthPiece / 3, 0, 2 * Math.PI);
+                ctx.fillStyle = 'yellow';
+                ctx.fill();
+                ctx.closePath();
+            }
+        });
+    });
 }
 renderGameboard(gameboard);
--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('canvas') as HTMLCanvasElement
 const canvasContainer = document.querySelector('div') as HTMLDivElement
-canvas.getContext('2d') as CanvasRenderingContext2D
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 
 // 2 indicates that theres a coin for pacman
 // 0 indicates empty filed
@@ -14,12 +14,12 @@ const gameboard = [
   [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
   [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
   [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
-  [2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2],
-  [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-  [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
-  [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-  [2, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 2],
-  [2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2],
+  [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 0, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+  [2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
   [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
   [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
@@ -35,12 +35,29 @@ canvas.width = canvasContainer.clientWidth
 canvas.height = canvasContainer.clientHeight
 
 window.addEventListener('resize', () => {
-  console.log('resize')
   canvas.width = canvasContainer.clientWidth
   canvas.height = canvasContainer.clientHeight
 })
 
 function renderGameboard (gameboard: number[][]): void {
+  const widthPiece = canvas.width / gameboard[0].length
+  const heightPiece = canvas.height / gameboard.length
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  gameboard.forEach((row, rowIndex) => {
+    row.forEach((cell, cellIndex) => {
+      if (cell === 2) {
+        // lest draw a circle
+        ctx.beginPath()
+        const x = (cellIndex * widthPiece) + (widthPiece / 2)
+        const y = (rowIndex * heightPiece) + (heightPiece / 2)
+        ctx.arc(x, y, widthPiece / 3, 0, 2 * Math.PI)
+        ctx.fillStyle = 'yellow'
+        ctx.fill()
+        ctx.closePath()
+      }
+    })
+  })
 }
 
 renderGameboard(gameboard)
